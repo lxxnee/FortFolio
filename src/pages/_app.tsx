4458c7d1ea0f5f6ea6d3384ce76d6110e734a01c
leeseolin <lsl9787@naver.com>
2024-04-29 20:40:34 +0900

    20240429 포트폴리오

---
+++ src/pages/_app.tsx
@@ -0,0 +1,61 @@
+import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+
+/**
+ * @description SEO를 위해 본인의 정보로 수정해주세요.
+ */
+const DEFAULT_SEO = {
+  title: "이서린 | Full-Stack Dev",
+  description: "안녕하세요, 풀스택 개발자 이서린 입니다.",
+  canonical: "https://www.naver.com/",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://www.naver.com/",
+    title: "이서린 | Full-Stack Dev",
+    site_name: "이서린 | Full-Stack Dev",
+    images: [
+      {
+        url: "/share.png",
+        width: 285,
+        height: 167,
+        alt: "이서린 | Full-Stack Dev",
+      },
+    ],
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: "application-name",
+      content: "홍길동 | Front-End Dev",
+    },
+    {
+      name: "msapplication-tooltip",
+      content: "홍길동 | Front-End Dev",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+  ],
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// const { fontFamily } = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      // NOTE: 본 템플릿은 대부분이 모노톤으로 이루어져 있습니다. primary 컬러만 수정하여 사용하시는 것을 권장드립니다.
+      colors: {
+        GRAY_LIGHT: "#f1f3f5",
+        GRAY: "#adb5bd",
+        GRAY_HEAVY: "#868e96",
+        GRAY_EXTRAHEAVY: "#495057",
+        BLACK: "#212529",
+        /**
+         * @description selection(드래그 블록)에 사용되는 컬러입니다.
+         */
+        PRIMARY_LIGHT: "#fad0c4",
+
+        /**
+         * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
+         */
+        PRIMARY: "#fad0c4",
+
+        /**
+         * @description code tag, link hover icon에 사용되는 컬러입니다.
+         */
+        PRIMARY_HEAVY: "#E09E46",
+
+        /**
+         * @description 페이지 최상단 gradient의 시작 컬러입니다.
+         */
+        GRADIENT_FROM: "#fad0c4",
+
+        /**
+         * @description 페이지 최상단 gradient의 종료 컬러입니다.
+         */
+        GRADIENT_TO: "#ffd1ff",
+      },
+    },
+  },
+  plugins: [],
+};
